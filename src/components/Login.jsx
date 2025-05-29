3683c7d431d3e922054c88731b4fc65dcad3035e
AngelJ-coder <joelredimidos@gmail.com>
2025-05-28 23:31:09 -0400

    Register,...

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,42 @@
+// src/components/Login.jsx
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const mockUsers = [
-    { username: 'juan', password: '1234', name: 'Juan Perez' },
-];
-
 export default function Login({ onLogin }) {
-    const [form, setForm] =  useState({ username: '', password: '' });
+  const [form, setForm] = useState({ username: '', password: '' });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
-        const user = mockUsers.find(u => u.username === form.username && u.password === form.password);
-        if (user) {
-            onLogin(user);
-        }
-        else {
-            alert('credenciales incorrectas');
-        }
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Iniciar Sesion</h2>
-            <input type="text" placeholder='Nombre de Usuario' onChange={e => setForm({...form, username: e.target.value})} />
-            <input type="password" placeholder='Contraseña' onChange={e => setForm({...form, password: e.target.value})}/>
-            <button type="submit">Iniciar</button>
-            <p>No tienes cuenta? <Link to='/register'>Registrate</Link></p>
-        </form>
+    const user = users.find(
+      (u) => u.username === form.username && u.password === form.password
     );
+
+    if (user) {
+      onLogin(user);
+    } else {
+      alert("Credenciales incorrectas");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Iniciar Sesión</h2>
+      <input
+        type="text"
+        placeholder="Nombre de Usuario"
+        onChange={(e) => setForm({ ...form, username: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Contraseña"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+      <button type="submit">Iniciar</button>
+      <p>
+        ¿No tienes cuenta? <Link to="/register">Regístrate</Link>
+      </p>
+    </form>
+  );
 }
